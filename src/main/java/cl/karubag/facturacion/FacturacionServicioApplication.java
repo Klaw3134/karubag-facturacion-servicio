cf3b8a7f9da21a3631ad197e753f605936fa6296
klaw3134 <cgnavarrete3134@gmail.com>
2026-05-28 20:02:15 -0400

    agrega clase principal FacturacionServicioApplication

---
+++ src/main/java/cl/karubag/facturacion/FacturacionServicioApplication.java
@@ -0,0 +1,12 @@
+package cl.karubag.facturacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturacionServicioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FacturacionServicioApplication.class, args);
+    }
+}
